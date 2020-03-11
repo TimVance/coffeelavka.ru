b0d2b91ef9af77715e8f8ee1fa917933972add6a
TimVance <2DngxvshA6GUGcQ>
2020-03-11 20:10:48 +0300

    Список ожиданий

---
+++ my/modules/wait/wait.install.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Установка модуля
+ *
+ * @package    DIAFAN.CMS
+ * @author     diafan.ru
+ * @version    6.0
+ * @license    http://www.diafan.ru/license.html
+ * @copyright  Copyright (c) 2003-2016 OOO «Диафан» (http://www.diafan.ru/)
+ */
+
+if (! defined('DIAFAN'))
+{
+    $path = __FILE__; $i = 0;
+    while(! file_exists($path.'/includes/404.php'))
+    {
+        if($i == 10) exit; $i++;
+        $path = dirname($path);
+    }
+    include $path.'/includes/404.php';
+}
+
+class Wait_install extends Install
+{
+    /**
+     * @var string название
+     */
+    public $title = "Список ожидания";
+
+
+    /**
+     * @var array записи в таблице {modules}
+     */
+    public $modules = array(
+        array(
+            "name" => "wait",
+            "admin" => true,
+            "site" => true,
+            "site_page" => true,
+        ),
+    );
+
+    /**
+     * @var array меню административной части
+     */
+    public $admin = array(
+        array(
+            "name" => "Список ожидания",
+            "rewrite" => "wait",
+            "group_id" => "2",
+            "sort" => 18,
+            "act" => true,
+            "docs" => "",
+        ),
+    );
+
+}
