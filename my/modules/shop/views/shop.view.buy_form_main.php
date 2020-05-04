f1705f16b7f71b6bd7a9f51dc785b3b9e2dd7ce8
TimVance <2DngxvshA6GUGcQ>
2020-05-04 13:26:46 +0300

    Вывел только вес в списке и вернул фотки

--- my/modules/shop/views/shop.view.buy_form_main.php
+++ my/modules/shop/views/shop.view.buy_form_main.php
@@ -106,7 +106,7 @@ if ($result["row"]["price_arr"])
 						{
 							if(! $select)
 							{
-								$select = '<div class="option-list-param">'.$param["name"].' <select name="param'.$param["id"].'" class="shop-dropdown inpselect'.($result["row"]["param_multiple"][$param["id"]][$value["id"]] == 'depend' ? ' depend_param js_shop_depend_param' : '').'">';
+								$select = '<div style="display: none" class="option-list-param">'.$param["name"].' <select name="param'.$param["id"].'" class="shop-dropdown inpselect'.($result["row"]["param_multiple"][$param["id"]][$value["id"]] == 'depend' ? ' depend_param js_shop_depend_param' : '').'">';
 							}
 
 							$select .= '<option value="'.$value["id"].'"'
